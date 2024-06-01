b8edaecb3b84733615c8b0dab871733c5bdbf399
michael <michael_muehlberger@me.com>
2024-06-01 23:53:09 +0200

    Initial Commit

---
+++ iOSAppMuehlberger/iOSAppMuehlbergerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppMuehlbergerApp.swift
+//  iOSAppMuehlberger
+//
+//  Created by Michael Mühlberger on 01.06.24.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppMuehlbergerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
